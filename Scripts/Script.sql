3d64cdcee079ce532fd2e4cefdd6ff9d39aece70
bomi <bomiisworldstar@gmail.com>
2017-06-08 21:06:11 +0900

    [170608] 오답풀이 할 차례

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -43,25 +43,24 @@ ALTER TABLE omori.most_test
 	MODIFY COLUMN ms_no INTEGER NOT NULL AUTO_INCREMENT;
 
 -- 오답노트
-CREATE TABLE omori.notes (
+CREATE TABLE omori.note (
 	note_no      INTEGER     NOT NULL, -- 오답노트번호
 	uid          VARCHAR(50) NOT NULL, -- 아이디
 	tno          INTEGER     NOT NULL, -- 자격증번호
 	tq_no        INTEGER     NOT NULL, -- 문제번호
-	sa_no        INTEGER     NULL,     -- 선택한답번호
 	note_content TEXT        NOT NULL, -- 오답노트풀이내용
 	note_memo    TEXT        NULL,     -- 오답노트추가메모
 	note_date    TIMESTAMP   NOT NULL  -- 오답노트작성날짜
 );
 
 -- 오답노트
-ALTER TABLE omori.notes
-	ADD CONSTRAINT PK_notes -- 오답노트 기본키
+ALTER TABLE omori.note
+	ADD CONSTRAINT PK_note -- 오답노트 기본키
 		PRIMARY KEY (
 			note_no -- 오답노트번호
 		);
 
-ALTER TABLE omori.notes
+ALTER TABLE omori.note
 	MODIFY COLUMN note_no INTEGER NOT NULL AUTO_INCREMENT;
 
 -- 문제
@@ -109,11 +108,11 @@ CREATE TABLE omori.grade (
 	uid          VARCHAR(50)  NOT NULL, -- 아이디
 	tno          INTEGER      NOT NULL, -- 자격증번호
 	grade        INTEGER      NOT NULL, -- 점수
-	g_low        VARCHAR(100) NULL,     -- 점수가낮은과목
-	g_low_grade  INTEGER      NULL,     -- 점수가낮은과목의점수
-	g_high       VARCHAR(100) NULL,     -- 점수가높은과목
-	g_high_grade INTEGER      NULL,     -- 점수가높은과목의점수
-	g_date       VARCHAR(50)  NOT NULL  -- 시험친날짜
+	g_low        VARCHAR(100) NOT NULL, -- 점수가낮은과목
+	g_low_grade  INTEGER      NOT NULL, -- 점수가낮은과목의점수
+	g_high       VARCHAR(100) NOT NULL, -- 점수가높은과목
+	g_high_grade INTEGER      NOT NULL, -- 점수가높은과목의점수
+	g_date       TIMESTAMP    NOT NULL  -- 시험친날짜
 );
 
 -- 성적
@@ -142,14 +141,14 @@ ALTER TABLE omori.user
 			uid -- 아이디
 		);
 
+		
 -- 사용자가 선택한 답
 CREATE TABLE omori.selected_answer (
 	sa_no     INTEGER     NOT NULL, -- 선택한답번호
-	tno       INTEGER     NULL,     -- 자격증번호
 	uid       VARCHAR(50) NULL,     -- 아이디
 	tq_no     INTEGER     NULL,     -- 문제번호
 	sa_answer INTEGER     NULL,     -- 선택한답
-	sa_date   VARCHAR(50) NULL      -- 시험친날짜
+	sa_date   TIMESTAMP   NULL      -- 시험친날짜
 );
 
 -- 사용자가 선택한 답
@@ -162,11 +161,34 @@ ALTER TABLE omori.selected_answer
 ALTER TABLE omori.selected_answer
 	MODIFY COLUMN sa_no INTEGER NOT NULL AUTO_INCREMENT;
 
+
+	
 -- 예시이미지
 CREATE TABLE omori.image (
 	tq_no     INTEGER      NULL,     -- 문제번호
 	imgsource VARCHAR(100) NOT NULL  -- 이미지이름
 );
+
+-- 시험본직후성적
+CREATE TABLE omori.nowgrade (
+	ng_no      INTEGER      NOT NULL, -- 시험본직후성적번호
+	uid		   VARCHAR(50)	null,	  -- 아이디
+	tno        INTEGER      NOT NULL, -- 자격증번호
+	tq_subject VARCHAR(100) NOT NULL, -- 과목명
+	nowgrade   INTEGER      NOT NULL, -- 점수(개수)
+	ng_count   INTEGER		NOT null, -- 과목의총개수
+	ng_date    TIMESTAMP    NOT NULL  -- 시험친날짜
+);
+
+-- 시험본직후성적
+ALTER TABLE omori.nowgrade
+	ADD CONSTRAINT PK_nowgrade -- 시험본직후성적 기본키
+		PRIMARY KEY (
+			ng_no -- 시험본직후성적번호
+		);
+
+ALTER TABLE omori.nowgrade
+	MODIFY COLUMN ng_no INTEGER NOT NULL AUTO_INCREMENT;
 
 -- 보기
 ALTER TABLE omori.testexample
@@ -199,7 +221,7 @@ ALTER TABLE omori.most_test
 		)on delete cascade on update cascade;
 
 -- 오답노트
-ALTER TABLE omori.notes
+ALTER TABLE omori.note
 	ADD CONSTRAINT FK_user_TO_notes -- 사용자 -> 오답노트
 		FOREIGN KEY (
 			uid -- 아이디
@@ -209,8 +231,8 @@ ALTER TABLE omori.notes
 		)on delete cascade on update cascade;
 
 -- 오답노트
-ALTER TABLE omori.notes
-	ADD CONSTRAINT FK_testname_TO_notes -- 자격증이름 -> 오답노트
+ALTER TABLE omori.note
+	ADD CONSTRAINT FK_testname_TO_note -- 자격증이름 -> 오답노트
 		FOREIGN KEY (
 			tno -- 자격증번호
 		)
@@ -219,23 +241,13 @@ ALTER TABLE omori.notes
 		)on delete cascade on update cascade;
 
 -- 오답노트
-ALTER TABLE omori.notes
-	ADD CONSTRAINT FK_testquestion_TO_notes -- 문제 -> 오답노트
+ALTER TABLE omori.note
+	ADD CONSTRAINT FK_testquestion_TO_note -- 문제 -> 오답노트
 		FOREIGN KEY (
 			tq_no -- 문제번호
 		)
 		REFERENCES omori.testquestion ( -- 문제
 			tq_no -- 문제번호
-		)on delete cascade on update cascade;
-
--- 오답노트
-ALTER TABLE omori.notes
-	ADD CONSTRAINT FK_selected_answer_TO_notes -- 사용자가 선택한 답 -> 오답노트
-		FOREIGN KEY (
-			sa_no -- 선택한답번호
-		)
-		REFERENCES omori.selected_answer ( -- 사용자가 선택한 답
-			sa_no -- 선택한답번호
 		)on delete cascade on update cascade;
 
 -- 문제
@@ -288,20 +300,11 @@ ALTER TABLE omori.selected_answer
 			uid -- 아이디
 		)on delete cascade on update cascade;
 
--- 사용자가 선택한 답
-ALTER TABLE omori.selected_answer
-	ADD CONSTRAINT FK_testname_TO_selected_answer -- 자격증이름 -> 사용자가 선택한 답
-		FOREIGN KEY (
-			tno -- 자격증번호
-		)
-		REFERENCES omori.testname ( -- 자격증이름
-			tno -- 자격증번호
-		)on delete cascade on update cascade;
-		
-		
-
--- ----------------------------------------------------------------------------------------
 use omori;
+
+create index idx_tqno on selected_answer(tq_no);
+create index idx_uid on selected_answer(uid);
+-- ----------------------------------------------------------------------------------------
 
 insert into user(uid, upw, uemail, ujoindate, isadmin) values('test1', 'test1', 'test1@naver.com',now(), false);
 insert into user(uid, upw, uemail, ujoindate, isadmin) values('test2', 'test2', 'test2@naver.com',now(), false);
@@ -318,17 +321,23 @@ delete from testname;
 
 LOAD DATA LOCAL INFILE "E:\\workspace\\workspace_spring\\Omori_2\\DataFiles\\testquestion.txt" INTO TABLE testquestion 
 FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE "E:\\workspace\\workspace_spring\\Omori_2\\DataFiles\\testquestion2.txt" INTO TABLE testquestion 
+FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';
 insert into testquestion(tno, tq_subject, tq_subject_no, tq_small_no, tq_question, tq_answer, tq_per) 
 values(1, '데이터베이스', 1, 1, '문제1', '1', '10');
 insert into testquestion(tno, tq_subject, tq_subject_no, tq_small_no, tq_question, tq_answer, tq_per) 
 values(1, '데이터베이스', 1, 2, '문제2', '2', '20');
-select * from testquestion;
+-- 과목명만 모아 보기
+select distinct tq_subject from testquestion where tno = 1;
+-- 한 과목당 문제 개수
+select count(*) from testquestion where tno = 1 and tq_subject = '데이터베이스'; 
 -- 모의 시험
 select * from testquestion where tno = 1 order by tq_small_no;
 -- 과목별 시험
 select * from testquestion where tno = 1 and tq_subject_no = 1 order by tq_small_no;
 -- 한문제씩 풀기
 select * from testquestion where tno = 1 and tq_small_no = 1;
+
 delete from testquestion;
 alter table testquestion auto_increment = 1;
 
@@ -340,43 +349,62 @@ delete from image;
 
 LOAD DATA LOCAL INFILE "E:\\workspace\\workspace_spring\\Omori_2\\DataFiles\\testexample.txt" INTO TABLE testexample 
 FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE "E:\\workspace\\workspace_spring\\Omori_2\\DataFiles\\testexample2.txt" INTO TABLE testexample 
+FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';
 insert into testexample(tq_no, te_small_no, te_content) values(1, 1, '문제1번의 예시1');
 insert into testexample(tq_no, te_small_no, te_content) values(1, 2, '문제1번의 예시2');
 insert into testexample(tq_no, te_small_no, te_content) values(1, 3, '문제1번의 예시3');
 insert into testexample(tq_no, te_small_no, te_content) values(1, 4, '문제1번의 예시4');
 select * from testexample;
 -- 문제 1번의 객관식 보기
-select * from testexample where tq_no = 1 order by te_small_no;
+select * from testexample where tq_no = 101 order by te_small_no;
 -- 문제 1번의 보기와 답
 select e.* from testexample e inner join testquestion q on e.te_small_no = q.tq_answer
 where e.tq_no = 1 and q.tq_no = 1;
 delete from testexample;
 alter table testexample auto_increment = 1;
 
-
-insert into selected_answer(tno, tq_no, uid, sa_answer, sa_date) values(1, 5, 'test1', 1, '2017-05-31');
-insert into selected_answer(tno, tq_no, uid, sa_answer, sa_date) values(1, 'test2', 2, '2017-05-31');
--- 자격증번호 1에 대해 'test1'이 2017-05-31날 선택한 답
-select * from selected_answer where tno = 1 and sa_date='2017-05-31' and uid = 'test1' order by tq_no;
--- 문제 1번에 대해 2017-05-31일 test1유저가 선택한 답
-select * from selected_answer where tq_no = 1 and sa_date = '2017-05-31' and uid='test1' order by sa_no desc limit 1;
+insert into selected_answer(uid, tq_no, sa_answer, sa_date) values('test1', 1, 1, now());
+-- 문제 1번에 대해 제일 최근 test1유저가 선택한 답
+select * from selected_answer where tq_no = 1 and uid='test1' order by sa_date desc limit 1;
 -- 문제 1번에 대해 2017-05-31일 test2유저가 선택한 답과 정답이 일치하는지 확인
 select s.* from selected_answer s inner join testquestion q on s.sa_answer = q.tq_answer
 where s.tq_no = 1 and q.tq_no = 1 and s.uid = 'test2' and s.sa_date = '2017-05-31';
 delete from selected_answer;
 alter table selected_answer auto_increment = 1;
-select * from selected_answer;
+
+insert into nowgrade(uid, tno, tq_subject, nowgrade, ng_date) values('test1', 1, '데이터베이스', 18, now());
+insert into nowgrade(uid, tno, tq_subject, nowgrade, ng_date) values('test1', 1, '전자계산기구조', 18, now());
+insert into nowgrade(uid, tno, tq_subject, nowgrade, ng_date) values('test1', 1, '운영체제', 18, now());
+insert into nowgrade(uid, tno, tq_subject, nowgrade, ng_date) values('test1', 1, '소프트웨어공학', 18, now());
+insert into nowgrade(uid, tno, tq_subject, nowgrade, ng_date) values('test1', 1, '데이터통신', 18, now());
+
+-- 제일 최근에 본 시험의 점수
+select * from nowgrade where tno = 1 and tq_subject = '데이터베이스' and uid='test1' order by ng_date desc limit 1;
+select * from nowgrade;
+
+delete from nowgrade;
+alter table nowgrade auto_increment = 1;
 
 insert into grade(uid, tno, grade, g_low, g_low_grade, g_high, g_high_grade, g_date) values('test1', 1, 75, '데이터베이스', 17, '전자계산기구조', 18, '2017-05-31');
 insert into grade(uid, tno, grade, g_low, g_high, g_date) values('test1', 1, 67, '데이터베이스', '전자계산기구조', '2017-05-30');
 insert into grade(uid, tno, grade, g_low, g_high, g_date) values('test1', 1, 80, '데이터베이스', '전자계산기구조', '2017-05-31');
 insert into grade(uid, tno, grade, g_low, g_high, g_date) values('test1', 1, 80, '데이터베이스', '전자계산기구조', '2017-05-31');
 -- 'test1'이 최근 본 시험 성적 순으로 정렬
-select * from grade where uid = 'test1' order by g_date desc, g_no desc;
+select * from grade where uid = 'test2' order by g_date desc, g_no desc;
 select * from grade;
 
-insert into notes(uid, tno, tq_no, sa_no, note_content, note_memo, note_date) values('test1', 1, 1, 1, '오답풀이', null, now());
-select * from notes;
+insert into note(uid, tno, tq_no, note_content, note_memo, note_date) values('test1', 1, 1, '문제 1의 오답풀잉', '틀렸던 이유', now());
+-- test1유저가 tno가 1인 문제에 오답풀이를 달은 모든 리스트
+select * from note where uid='test1' and tno = 1 order by tq_no;
+-- test1유저가 tno가 1이고 tq_no가 1인 문제에 오답풀이단 것을 가져옴 
+select * from note where uid='test1' and tno=1 and tq_no = 1 order by note_date desc limit 1;
+-- 수정
+update note set note_content = '11', note_memo = '11' where note_no = 1;
+-- 삭제
+delete from note where note_no = 1;
+
+select * from note;
 
 insert into most_test(uid, tno, ms_count) values('test1', 1, 5);
 select * from most_test;
---
+++ src/main/java/com/dgit/persistence/NoteDaoImpl.java
@@ -0,0 +1,51 @@
+package com.dgit.persistence;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.dgit.domain.NoteVO;
+
+@Repository
+public class NoteDaoImpl implements NoteDao{
+	@Autowired
+	private SqlSession session;
+	
+	private static final String namespace = "com.dgit.mapper.NoteMapper";
+
+	@Override
+	public void insertNote(NoteVO vo) throws Exception {
+		session.insert(namespace + ".insertNote", vo);
+	}
+
+	@Override
+	public List<NoteVO> selectAllNoteByTno(String uid, int tno) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("uid", uid);
+		map.put("tno", tno);
+		return session.selectList(namespace+".selectAllNoteByTno", map);
+	}
+
+	@Override
+	public NoteVO selectOneNoteByTnoTqno(String uid, int tno, int tq_no) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("uid", uid);
+		map.put("tno", tno);
+		map.put("tq_no", tq_no);
+		return session.selectOne(namespace + ".selectOneNoteByTnoTqno", map);
+	}
+
+	@Override
+	public void updateNote(NoteVO vo) throws Exception {
+		session.update(namespace + ".updateNote", vo);
+	}
+
+	@Override
+	public void deleteNote(int note_no) throws Exception {
+		session.delete(namespace+".deleteNote", note_no);
+	}
+}
